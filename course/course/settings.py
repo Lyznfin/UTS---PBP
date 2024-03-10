ea53487e2516a92d05dd7f7a369d740e1aae241b
manzil <manzil.akbar@gmail.com>
2024-03-10 22:15:24 +0700

    I forgot

--- course/course/settings.py
+++ course/course/settings.py
@@ -123,7 +123,14 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+CRISPY_ALLOWED_TEMPLATE_PACK = 'bootstrap5'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/login'
